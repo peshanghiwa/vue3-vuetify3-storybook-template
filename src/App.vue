4f245c3e8b8467f4c41bc198519e4378220e1c92
Peshang Hiwa <51766800+peshanghiwa@users.noreply.github.com>
2023-02-25 14:52:18 +0300

    added vuetify configurations

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,46 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useTheme } from "vuetify";
+
+const theme = useTheme();
+
+const switchTheme = () => {
+  theme.global.name.value = theme.global.current.value.dark ? "light" : "dark";
+};
+</script>
 
 <template>
-  <v-app theme="dark">
+  <v-app>
     <v-main>
-      <v-container>
-        <v-row>
-          <v-col cols="12">
-            <v-card>
-              <v-card-title>
-                <h1>Vue 3 + Vite + Vuetify 3</h1>
-              </v-card-title>
-              <v-card-text>
-                <p>
-                  This is a simple example of how to use Vue 3, Vite and Vuetify
-                  3 together.
-                </p>
-              </v-card-text>
-            </v-card>
+      <v-container class="my-10">
+        <v-row justify="center" align="center">
+          <v-col cols="auto">
+            <img src="../asstes/vue.png" height="200" />
+          </v-col>
+          <v-col cols="auto">
+            <img src="../asstes/vuetify.svg" height="200" />
+          </v-col>
+          <v-col cols="auto">
+            <img src="../asstes/storybook.svg" height="200" />
           </v-col>
         </v-row>
+
+        <v-card class="description pa-5 mt-10 text-center">
+          <v-card-title>
+            <h1>Vue 3 + Vuetify 3 + Storybook 6</h1>
+          </v-card-title>
+          <v-card-text>
+            <p>
+              This is a starter template for Vue 3 + Vuetify 3 + Storybook 6
+            </p>
+            <p>Run <code>npm run storybook</code> to start Storybook</p>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn color="primary mx-auto" variant="flat" @click="switchTheme">
+              Switch theme
+            </v-btn>
+          </v-card-actions>
+        </v-card>
       </v-container>
     </v-main>
   </v-app>
 </template>
-
-<style scoped></style>
--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,10 +1,45 @@
-// Vuetify
-import "vuetify/styles";
 import { createVuetify } from "vuetify";
+import type { ThemeDefinition } from "vuetify";
+import "vuetify/styles";
+import "@mdi/font/css/materialdesignicons.css";
+
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
+
+const lightTheme: ThemeDefinition = {
+  dark: false,
+  colors: {
+    primary: "#d35400",
+    secondary: "#8e44ad",
+    background: "#ecf0f1",
+    error: "#c0392b",
+    info: "#2980b9",
+    success: "#27ae60",
+    warning: "#f1c40f",
+  },
+};
+
+const darkTheme: ThemeDefinition = {
+  dark: true,
+  colors: {
+    primary: "#d35400",
+    secondary: "#8e44ad",
+    background: "#2f3640",
+    error: "#c0392b",
+    info: "#2980b9",
+    success: "#27ae60",
+    warning: "#f1c40f",
+  },
+};
 
 export default createVuetify({
   components,
   directives,
+  theme: {
+    defaultTheme: "light",
+    themes: {
+      light: lightTheme,
+      dark: darkTheme,
+    },
+  },
 });
